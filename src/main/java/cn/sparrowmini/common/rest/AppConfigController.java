ae9993946abf2389a91d9ef6903c84f24adad723
Fan Sword <fanmeijian@gmail.com>
2025-08-06 03:19:30 +0800

    优化通用方法

--- src/main/java/cn/sparrowmini/common/rest/AppConfigController.java
+++ src/main/java/cn/sparrowmini/common/rest/AppConfigController.java
@@ -1,28 +1,56 @@
 package cn.sparrowmini.common.rest;
 
 import cn.sparrowmini.common.model.AppConfig;
+import cn.sparrowmini.common.model.AppConfigAttachment;
+import cn.sparrowmini.common.repository.AppConfigAttachmentRepository;
+import cn.sparrowmini.common.repository.AppConfigRepository;
 import cn.sparrowmini.common.service.CommonJpaService;
 import cn.sparrowmini.common.service.SimpleJpaFilter;
+import cn.sparrowmini.common.view.AppConfigInfo;
+import cn.sparrowmini.common.view.AppConfigView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("app-configs")
 public class AppConfigController {
 
     @Autowired
-    private CommonJpaService commonJpaService;
+    private AppConfigRepository appConfigRepository;
+
+    @Autowired
+    private AppConfigAttachmentRepository appConfigAttachmentRepository;
 
     @GetMapping("")
     @ResponseBody
-    public Page<AppConfig> getEntityList(Pageable pageable, String filter){
-        return commonJpaService.getEntityList(AppConfig.class, pageable, filter);
+    public Page<AppConfigView> getEntityList(Pageable pageable, String filter){
+        return appConfigRepository.findAll(pageable, filter, AppConfigView.class);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public AppConfigInfo getEntityList(@PathVariable String id){
+        return appConfigRepository.findByIdProjection(id, AppConfigInfo.class).orElseThrow();
+    }
+
+    @GetMapping("/attachments/{attachmentId}")
+    @ResponseBody
+    public AppConfigAttachment getAttachment(@PathVariable String attachmentId){
+        return appConfigAttachmentRepository.findById(attachmentId).orElseThrow();
+    }
+
+    @DeleteMapping("/attachments")
+    @ResponseBody
+    public void deleteAttachment(@RequestParam("id") Set<String> attachmentIds){
+        appConfigAttachmentRepository.deleteByIds(attachmentIds);
     }
 
 }
--- src/main/java/cn/sparrowmini/common/rest/CommonJpaController.java
+++ src/main/java/cn/sparrowmini/common/rest/CommonJpaController.java
@@ -1,15 +1,22 @@
 package cn.sparrowmini.common.rest;
 
+import cn.sparrowmini.common.model.ApiResponse;
 import cn.sparrowmini.common.service.CommonJpaService;
 import cn.sparrowmini.common.service.SimpleJpaFilter;
+import cn.sparrowmini.common.util.JpaUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("common-jpa-controller")
@@ -21,37 +28,61 @@ public class CommonJpaController {
     @PostMapping("/{className}")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.CREATED)
-    public void saveEntity(@PathVariable String className,@RequestBody Object[] body){
-        commonJpaService.saveEntity(className,body);
+    public ApiResponse<?> saveEntity(@PathVariable String className, @RequestBody List<Object> body){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Class<?> clazz = Class.forName(className);
+            // 关键：构造一个 List<clazz> 的 JavaType
+            JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, clazz);
+            List<?> ids = commonJpaService.saveEntity(new ObjectMapper().convertValue(body, type));
+            return new ApiResponse<>(ids);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @PatchMapping("/{className}")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void updateEntity(@PathVariable String className,@RequestBody List<Map<String, Object>> entities){
-        commonJpaService.updateEntity(className,entities);
+        try {
+            Class<?> clazz = Class.forName(className);
+            commonJpaService.updateEntity(clazz,entities);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-    @PatchMapping("/{className}/delete")
+
+    @DeleteMapping("/{className}")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void deleteEntity(@PathVariable String className,@RequestBody Object[] ids){
-        commonJpaService.deleteEntity(className, ids);
+    @Transactional
+    public void deleteEntity(@PathVariable String className,@RequestParam("id") Set<Object> ids){
+        try {
+            Class<?> clazz = Class.forName(className);
+            commonJpaService.deleteEntity(clazz, ids);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-    @PostMapping( "/{className}/get-by-id")
+    @GetMapping( "/{className}/get-by-id")
     @ResponseBody
-    public Object getEntity(@PathVariable String className,@RequestBody Map<String, Object> map){
-        return commonJpaService.getEntity(className, map.get("id"));
-
+    public Object getEntity(@PathVariable String className,@RequestParam String id){
+        try {
+            Class<?> clazz = Class.forName(className);
+            Class<?> idClass = JpaUtils.getIdType(clazz);
+            Object id_ = new ObjectMapper().convertValue(id, idClass);
+            return commonJpaService.getEntity(clazz, id_);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-
-    @PostMapping("/{className}/filter")
-    @ResponseBody
-    public Page<Object> getEntityList(@PathVariable String className, Pageable pageable,@RequestBody List<SimpleJpaFilter> filterList){
-        return commonJpaService.getEntityList(className, pageable, filterList);
-    }
 
     @GetMapping("/{className}")
     @ResponseBody
