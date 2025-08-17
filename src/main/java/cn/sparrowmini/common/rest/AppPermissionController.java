fba84d6615b6d04a1e3cc12bd76cc65a6496240a
Fan Sword <fanmeijian@gmail.com>
2025-08-18 00:06:17 +0800

    增加页面元素功能配置

--- src/main/java/cn/sparrowmini/common/rest/AppPermissionController.java
+++ src/main/java/cn/sparrowmini/common/rest/AppPermissionController.java
@@ -1,11 +1,17 @@
 package cn.sparrowmini.common.rest;
 
+import cn.sparrowmini.common.CurrentUser;
+import cn.sparrowmini.common.constant.PermissionTypeEnum;
 import cn.sparrowmini.common.service.MenuService;
 import cn.sparrowmini.common.service.ModelService;
+import cn.sparrowmini.common.service.PageElementService;
 import cn.sparrowmini.common.service.ScopeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("permissions")
@@ -15,6 +21,9 @@ public class AppPermissionController {
 
     @Autowired
     private ModelService modelService;
+
+    @Autowired
+    private PageElementService pageElementService;
 
     @Autowired
     private MenuService menuService;
@@ -31,6 +40,13 @@ public class AppPermissionController {
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void synchronizeModel(){
         modelService.synchronize();
+    }
+
+    @GetMapping("/page-elements/permissions")
+    @ResponseBody
+    public Map<String, PermissionTypeEnum> getPageElementPermission(@RequestParam("id") Set<String> ids){
+
+        return pageElementService.pageElementByPage(ids, CurrentUser.getUserInfo());
     }
 
 }
